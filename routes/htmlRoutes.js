b7b64fe92515e26880568ee034aeda69fdec37f1
virus0071 <virus00719@gmail.com>
2018-10-05 11:49:27 -0400

    react starter

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,8 @@ var path = require("path");
 
 module.exports = function(app) {
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/login.html"))
-  });
-
-  app.get("/home", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/home.html"))
+  router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 
   // Render 404 page for any unmatched routes
